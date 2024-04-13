99242c3c27ada23851a028aa029221f19d7f4df6
ALazzyDragoCoder <amanrichhariya012@gmail.com>
2024-04-14 00:46:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage ="dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage ="dice" + randomNumber2 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource);
+
+
+// Change Heading to the winner 
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🏆Player 1 Wins!🚀";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "🏆Player 2 Wins!🚀";
+}
+else {
+    document.querySelector("h1").innerHTML = "🏆Both are the winners or Draw🚀";
+}
